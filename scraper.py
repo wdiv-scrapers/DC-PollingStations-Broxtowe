612cf38ca9bf34c70493d9b66d2a7db19a621bd5
GeoWill <roper.william@gmail.com>
2021-03-19 22:23:31 +0000

    Quieter scraper

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@ from dc_base_scrapers.geojson_scraper import GeoJsonScraper
 from dc_base_scrapers.hashonly_scraper import HashOnlyScraper
 
 
-search_url = "https://opendata.arcgis.com/api/v3/datasets?q=polling&filter%5Bowner%5D=Broxtowe_GIS"
+search_url = "https://opendata.arcgis.com/api/v3/datasets?q=polling&filter%5Bowner%5D=Broxtowe_GIS&fields[datasets]=name,url"
 stations_url = "https://opendata.arcgis.com/datasets/a3da8ff112a647fe857a45c9996488fd_3.geojson"
 districts_url = "https://opendata.arcgis.com/datasets/ace0ecbc348a41b5b804557f6a68f575_1.geojson"
 council_id = 'BRT'
